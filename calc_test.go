a709cc90650b72da00d4657dab5a2b5e4b811500
SOK14 <yoshihiro_tsujii@yahoo.co.jp>
2022-12-19 14:34:41 +0900

    update test

--- calc_test.go
+++ calc_test.go
@@ -12,3 +12,26 @@ func TestPlusSimply(t *testing.T) {
 
 	assert.Equal(t, expect, result)
 }
+
+func TestPlusAssertFailContinue(t *testing.T) {
+	result1 := Plus(1, 3)
+	expect1 := 4
+
+	assert.Equal(t, expect1, result1)
+
+	result2 := Plus(1, 3)
+	badExpect2 := 5 // miss
+
+	assert.Equal(t, badExpect2, result2)
+
+	result3 := Plus(3, 5)
+	expect3 := 8
+
+	assert.Equal(t, expect3, result3, "not equal")
+
+	result4 := Plus(3, 5)
+	badExpect4 := 9 // miss
+
+	assert.Equal(t, badExpect4, result4, "not equal")
+
+}
