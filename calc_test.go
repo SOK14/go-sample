6c5c9e9c7498fc6764466ad986542578d503d386
SOK14 <yoshihiro_tsujii@yahoo.co.jp>
2022-12-19 14:24:42 +0900

    update calc test

--- calc_test.go
+++ calc_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlusSimply(t *testing.T) {
+	result := Plus(1, 3)
+	expect := 4
+
+	assert.Equal(t, expect, result)
+}
